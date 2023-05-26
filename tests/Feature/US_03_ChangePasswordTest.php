cc1a7e080236dbb28f53f0c58383ea3017cd5648
trisnawahyudiii <wayantrisna79@gmail.com>
2023-05-27 02:48:07 +0800

    add US_03

--- tests/Feature/US_03_ChangePasswordTest.php
+++ tests/Feature/US_03_ChangePasswordTest.php
@@ -4,17 +4,47 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class US_03_ChangePasswordTest extends TestCase
 {
     // use RefreshDatabase;
 
-    public function test_example()
+    public function test_gutu_dapat_mengubah_password()
     {
-        
-        $response = $this->get('/');
+        Auth::login($this->guru);
 
-        $response->assertStatus(302);
+        $oldPassword = $this->guru->password;
+
+        $response = $this->put('/api/guru/' . $this->guru->id, [
+            'password' => 'password_guru_baru'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson(['message' => 'success update data user']);
+        $this->assertDatabaseMissing('gurus', [
+            'id' => $this->orang_tua->id,
+            'password' => $oldPassword
+        ]);
+    }
+
+    public function test_orang_tua_dapat_mengubah_password()
+    {
+        Auth::login($this->orang_tua);
+
+        $oldPassword = $this->orang_tua->password;
+
+        $response = $this->put('/api/orang-tua/' . $this->orang_tua->id, [
+            'password' => 'password_orang_tua_baru'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson(['message' => 'success update data user']);
+        $this->assertDatabaseMissing('orang_tuas', [
+            'id' => $this->orang_tua->id,
+            'password' => $oldPassword
+        ]);
     }
 }
