f20c139fb12af8d8f8a0112dc14d02224e2daf61
trisnawahyudiii <wayantrisna79@gmail.com>
2023-06-03 21:58:15 +0800

    add US_22 and US_23

--- tests/Feature/US_23_TambahDataAktivitasTest.php
+++ tests/Feature/US_23_TambahDataAktivitasTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Feature;
 
-use App\Models\Aktivitas;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class US_22_LihatDataAktivitasTest extends TestCase
+class US_23_TambahDataAktivitasTest extends TestCase
 {
-    public function test_Admin_dapat_melihat_data_aktifitas()
+
+    public function test_admin_dapat_menambah_data_aktivitas()
     {
         // login
         $response = $this->from('/admin/loginIndex')->post('/admin/login', [
@@ -17,20 +17,18 @@ class US_22_LihatDataAktivitasTest extends TestCase
             'password' => "password_admin",
         ]);
 
-        // create aktivitas
         $request = [
             'name' => $this->faker()->jobTitle(),
         ];
         $response = $this->from('/aktivitas/create/index')->post('/aktivitas/create', $request);
 
-        // request
-        $aktivitas = Aktivitas::get();
-        $response = $this->get('/aktivitas/index');
+        // assert response
+        $response->assertStatus(302);
+        $response->assertSessionHas("success_create", "Berhasil Menambahkan Data");
 
-        // response assert
-        $response->assertStatus(200);
-        $response->assertViewIs('aktivitas.index');
-        $response->assertViewHas('data_aktivitas', $aktivitas);
-        $response->assertSee($aktivitas[0]->name);
+        // assert database
+        $this->assertDatabaseHas('aktivitas', [
+            "name" => $request["name"],
+        ]);
     }
 }
